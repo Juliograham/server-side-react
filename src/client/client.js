2c838ff6417456b43ccf88c98e9d1a791a9b14bc
Julio Graham <julioagraham@gmail.com>
2018-06-27 23:37:46 -0400

    Build custom axios instance for client side and add middleware to redux-thunk

--- src/client/client.js
+++ src/client/client.js
@@ -5,14 +5,24 @@ import ReactDOM from 'react-dom';
 import Home from './components/Home';
 import { BrowserRouter } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
+import axios from 'axios';
 import Routes from './Routes';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
 
+const axiosInstance = axios.create({
+    baseURL: '/api'
+});
 
-const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+
+const store = createStore(
+    reducers, 
+    window.INITIAL_STATE, 
+    applyMiddleware(thunk.withExtraArgument(axiosInstance))
+);
 
 ReactDOM.hydrate(
     <Provider store={store}>
